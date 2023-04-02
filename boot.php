7cdcb2e19992056c36d01581a3a13acf7f62c91e
Zoe Boyle <zboyle1@student.gsu.edu>
2023-04-01 20:09:28 -0400

    coded header

--- boot.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Include php files necessary to build every page
-include 'header.php';
-include 'footer.php';
-include 'functions.php';
-
-
-?>
--- header.php
+++ header.php
@@ -8,29 +8,59 @@ function isLoggedIn(): ?string {
     }
 }
 
-function send_header(?string $page) {
+function guest_nav() {
+    echo <<< GUESTLINKS
+    <li><a href = "">Login</a>
+    <li><a href = "">Sign Up</a>
+    <li><a href = "">Home</a>
+    GUESTLINKS;
+}
+
+function user_nav($user) {
+    echo <<< USERLINKS
+    <li><a href = "">Home</a>
+    <li><a href = "">Profile</a>
+    <li><a href = "">My Pets</a>
+    <li><a href = "">Shops</a>
+    <li><a href = "">Logout</a>
+    USERLINKS;
+}
+
+$user = isLoggedIn();
 ?>
-    <html>
+
+<html>
     <head>
-        <title>Virtual Pets<?php if($page != 'index') { echo "- $page"; } ?></title>
+        <title>Virtual Pets<?php echo (!isset($page) ? : "- $page"); ?></title>
         <!-- jQuery -->
-        <script src="jquery-3.6.1.min.js"></script>
+        <script src = "jquery-3.6.1.min.js"></script>
 	
         <!-- Foundation -->
-        <link rel="stylesheet" href="css/foundation.css">
-        <link rel="stylesheet" href="css/app.css">
+        <link rel = "stylesheet" href = "css/foundation.css">
+        <link rel = "stylesheet" href = "css/app.css">
 
-        <script src="js/vendor/jquery.js"></script>
-        <script src="js/vendor/what-input.js"></script>
-        <script src="js/vendor/foundation.js"></script>
-        <script src="js/app.js"></script>
+        <script src = "js/vendor/jquery.js"></script>
+        <script src = "js/vendor/what-input.js"></script>
+        <script src = "js/vendor/foundation.js"></script>
+        <script src = "js/app.js"></script>
     </head>
     <body>
+        <div class = "grid-x">
+           <div id = "cell">
+                Logo
+            </div>
+            <div class = "cell medium-6 large-4">
+                Welcome, <?php echo (!isset($user) ? "guest!" : "$user!")?>
+            </div>
+            <div class = "cell medium-6 large-8">
+                <ul class = "menu">
 <?php
-    $user = isLoggedIn;
-    if(!$user) {
-
+    if(!isset($user)) {
+        guest_nav();
+    } else {
+        user_nav($user);
     }
-
-}
 ?>
+                </ul>
+            </div>
+        </div>
