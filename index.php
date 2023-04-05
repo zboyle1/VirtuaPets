6bc5bef5f565245fabfbfba0e74a9e6eea89943a
Zoe Boyle <zboyle1@student.gsu.edu>
2023-04-04 21:37:45 -0400

    seperated ajax request

--- index.php
+++ index.php
@@ -3,9 +3,7 @@
     include 'footer.php';
 ?>
     <div class = "x-grid">
-        <div class = "cell">
-            
-
+        <div class = "cell" id = "content">
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus risus nunc, eleifend non erat eget, feugiat rhoncus est. Proin cursus orci ut nisl mattis, a pulvinar nisl porttitor. Maecenas scelerisque nunc augue, at scelerisque quam dignissim at. Integer aliquam imperdiet ex, at rhoncus nulla pellentesque ac. Integer sit amet suscipit leo. Curabitur vel felis sapien. Pellentesque interdum pulvinar metus, eget pellentesque libero mollis sed. Nulla sed convallis orci, sit amet viverra enim. Etiam finibus massa id ante consequat commodo. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Morbi porttitor tristique sodales. Duis sed erat erat.
 
 Suspendisse suscipit lectus sed quam accumsan imperdiet. Nullam at tincidunt diam. Mauris vitae nibh elementum, semper nibh non, placerat risus. Cras ut consectetur tellus. Ut aliquam est at eros euismod cursus. Quisque facilisis dolor erat, et sodales turpis rutrum et. Mauris turpis neque, malesuada ut eros vel, semper elementum ante. Etiam ut leo vitae arcu auctor porttitor in id quam. Nulla sit amet metus in elit maximus gravida. Aliquam ullamcorper vitae neque eget lacinia. Aenean dignissim nisl at ex cursus, sit amet blandit nisl fermentum. In tellus nulla, ullamcorper quis efficitur ut, mollis a orci. Integer id velit vel risus sodales finibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
