a447d1c9242c1aa0fa9b7d793fe88bd84abbee8c
Zoe Boyle <zboyle1@student.gsu.edu>
2023-04-14 15:07:24 -0400

    small layout fixes

--- places/createpet.php
+++ places/createpet.php
@@ -3,16 +3,16 @@
 	include '../header.php';
     include '../footer.php';
 ?>
-<div id = "content" class = "grid-x grid-padding-x grid-margin-x align-center">
+<div id = "content" class = "grid-x grid-padding-x grid-margin-x">
     <div class = "cell">
         <h3>Create a pet</h3>
     </div>
     <!-- image of chosen pet to the right -->
-    <div class = "cell small-4 medium-4 large-5">
-        <img src="/~zboyle1/assets/petimg/petplaceholder.png">
+    <div class = "cell small-5 medium-4 large-4 align-self-middle">
+        <img class = "thumbnail" src="/~zboyle1/assets/petimg/petplaceholder.png">
     </div>
     <!-- to the left: -->
-    <div class = "cell auto">
+    <div class = "cell small-7 medium-8 large-8">
         <form onsubmit="return(createpet())">
             <!-- text field for petname -->
 		    <label>Pet name:
@@ -34,10 +34,13 @@
                 </select>
             </label>
             <!-- radio buttons for gender -->
-  
+            <fieldset>
+            <legend>Pet gender</legend>
+                <input type="radio" name="gender" value="Male" id="male" required><label for="male">Male</label>
+                <input type="radio" name="gender" value="Female" id="female"><label for="female">Female</label>
+            </fieldset>
             <!-- submit button, calls ajax request to create new row in pet table -->
             <button class = "submit button">Create pet</button>
- 
             <div id="message"></div>
 	    </form>
     </div>
