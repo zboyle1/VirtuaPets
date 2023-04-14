2ce4e000557b76b0409542015088fcb11c48e7b4
Zoe Boyle <zboyle1@student.gsu.edu>
2023-04-14 13:55:53 -0400

    small login page layout fix

--- places/login.php
+++ places/login.php
@@ -4,14 +4,14 @@
     include '../footer.php';
 ?>
 <div id = "content" class = "grid-x grid-padding-x grid-margin-x">
+
     <div class="cell large-8 medium-6 small-4">  
     <form onsubmit="return(login())">
-
 		<label>Username
             <input type="text" id="user">
         </label>
 		<label>Password
-            <input type="text" id="pass"></br>
+            <input type="text" id="pass">
         </label>
         <button class = "submit button">Login</button>
         <a href = "/~zboyle1/places/signup.php" class="button secondary">Sign up</a>
