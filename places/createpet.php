ea6c0c2217fc9e1335e353b1a126720457f55d6c
Zoe Boyle <zboyle1@student.gsu.edu>
2023-04-14 02:33:49 -0400

    positioned divs, still need to center image

--- places/createpet.php
+++ places/createpet.php
@@ -3,13 +3,45 @@
 	include '../header.php';
     include '../footer.php';
 ?>
-<h2>Create a pet</h2>
-<!-- image of chosen pet to the right -->
-<!-- to the left: -->
-<!-- text field for petname -->
-<!-- drop downs for species and color -->
-<!-- radio buttons for gender -->
-<!-- submit button, calls ajax request to create new row in pet table -->
+<div id = "content" class = "grid-x grid-padding-x grid-margin-x align-center">
+    <div class = "cell">
+        <h3>Create a pet</h3>
+    </div>
+    <!-- image of chosen pet to the right -->
+    <div class = "cell small-4 medium-4 large-5">
+        <img src="/~zboyle1/petimg/petplaceholder.png">
+    </div>
+    <!-- to the left: -->
+    <div class = "cell auto">
+        <form onsubmit="return(createpet())">
+            <!-- text field for petname -->
+		    <label>Pet name:
+                <input type="text" id="user">
+            </label>
+            <!-- drop downs for species and color -->
+            <label>Species
+                <select id = "species" onchange = "updatecolorop()" autocomplete="off">
+                    <option value="">--Choose species--</option>
+                    <option value="dog">Dog</option>
+                    <option value="cat">Cat</option>
+                    <option value="ferret">Ferret</option>
+                    <option value="rabbit">Rabbit</option>
+                    <option value="frog">Frog</option>
+               </select>
+            </label>
+            <label>Color
+                <select id ="color">
+                </select>
+            </label>
+            <!-- radio buttons for gender -->
+  
+            <!-- submit button, calls ajax request to create new row in pet table -->
+            <button class = "submit button">Create pet</button>
+ 
+            <div id="message"></div>
+	    </form>
+    </div>
+</div>
 <?php
     send_footer();
 ?>
