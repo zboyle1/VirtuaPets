4f68076a32921ccb539519f54d12c57700df35ac
Zoe Boyle <zboyle1@student.gsu.edu>
2023-04-15 22:08:19 -0400

    added ccmments

--- ajax/loginajax.php
+++ ajax/loginajax.php
@@ -4,11 +4,14 @@ define( 'DB_USER', 'zboyle1' );
 define( 'DB_PASSWORD', 'zboyle1' );
 define( 'DB_HOST', 'localhost' );
 
+// Connect to SQL server
 $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
     
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
+
+// Login function
 
 function isvalid ($user, $pass) {	
     global $conn;
@@ -39,6 +42,8 @@ function logout() {
     setcookie("user","", time() - 3600, "/");
     echo '1';
 }
+
+// Signup
 
 function signup() {
     $user = $_POST['newuser'];
@@ -71,6 +76,44 @@ function signup() {
     echo '3';
 }
 
+// Show user information
+
+function showuser() {
+
+    global $conn;
+
+    $user = $_POST['user'];
+
+    $user_arr = array();
+
+    $select = "SELECT * FROM users WHERE username = '$user';";
+    $result = $conn->query($select);
+
+    if (mysqli_num_rows($result) == 0) {
+        echo '0';
+        return;
+    }
+
+    $row = mysqli_fetch_assoc($result);
+    $id = $row['id'];
+    $dob = $row['birthdate'];
+    $join = $row['joindate'];
+    $age = date_diff(date_create($dob), date_create('now'))->y;
+    $join = explode("-", $join);
+    
+    echo '<table  id = "usertable"><tr>' .
+         '<td align = "center">Username:</td>' .
+         '<td>' . $user . '</td>' .
+         '</tr><tr>' .
+         '<td align = "center">Age:</td>' .
+         '<td>' . $age . '</td>' .
+         '</tr><tr>' .
+         '<td align = "center">Join date:</td>' .
+         '<td>' . $join[1] . '/' . $join[2] . '/' . $join[0] . '</td>' .
+         '</tr></table>';
+}
+
+// Execute function
 
 $cmd = $_POST['cmd'];
 
@@ -80,6 +123,8 @@ if($cmd == 'login') {
     logout();
 } else if ($cmd == 'signup') {
     signup();
+} else if ($cmd == 'show') {
+    showuser();
 }
 
 
