9e715629872572eec550aec7a343cb2c6fed312c
Zoe Boyle <zboyle1@student.gsu.edu>
2023-04-17 21:34:12 -0400

    description finished

--- places/description.php
+++ places/description.php
@@ -1,24 +1,26 @@
 <?php
-    $page = "Site Description";
 	include '../header.php';
     include '../footer.php';
 ?>
-<div id = "content" class = "grid-x grid-padding-x grid-margin-x">
-    <div class ="cell">
-        <h3>Site Description</h3>
-    </div>
-    
-    <div class = "cell large-10">
-        <div class="callout">
-            <p style="margin-left: 1em;">
-                
-            </p>
+<div class="cell large-10">
+    <div class = "grid-x grid-padding-x ">
+        <div class ="cell">
+            <h3>Site Description</h3>
         </div>
+    
+        <div class = "cell">
+            <div class="callout">
+                <p style="margin-left: 1em;">
+                    The users of this site are gamers of any age, but the concept is targetted towards children and thus are the main users. I want people to have fun and enjoy their pets! The problem I am solving is the problem of children not having safe child-friendly spaces on the internet.
+                </p>
+            </div>
         
-        <div class="callout">
-            <p style="margin-left: 1em;">
-            
-            </p>
+            <div class="callout">
+                <h4 class="subheader">What I'd like to add</h4>
+                <p style="margin-left: 1em;">
+                    Simple games, more interactions with pets and more pets and colors, a chat function (in production, this would have moderation.)
+                </p>
+            </div>
         </div>
     </div>
 </div>
