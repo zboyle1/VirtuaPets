95e9d27fbf9de4904184569400933b2ce9daf55d
Zoe Boyle <zboyle1@student.gsu.edu>
2023-04-17 20:20:32 -0400

    about page complete

--- places/about.php
+++ places/about.php
@@ -1,43 +1,47 @@
 <?php
-    $page = "About";
 	include '../header.php';
     include '../footer.php';
 ?>
-<div id = "content" class = "grid-x grid-padding-x grid-margin-x">
-    <div class ="cell">
-        <h3>About Us</h3>
-    </div>
-    
-    <div class = "cell large-10">
-        <div class="callout">
-            <h4 class = "subheader">About the creator</h4>
-            <p style="margin-left: 1em;">
-                Hello! My name is Zoe and I'm a computer science major. I'm 26 years old and I plan to graduate either this summer or fall. In my free time I like to draw and play video games. 
-            </p>
+<div class="cell medium-10 large-10">
+    <div id="shop" class="grid-x grid-padding-x grid-margin-x">
+        <div class="cell">
+            <h3>About Us</h3>
         </div>
-        
-        <div class="callout">
-            <h4 class = "subheader">About the project</h4>
-            <p style="margin-left: 1em;">
-                This project is a petsite, where a user can create and take care of virtual pets! I chose this project because I grew up on petsites, but most of them have been taken offline or were heavily dependent on flash and java. I thought it would be cool to bring modern functionalities to the concept.
-            </p>
-        </div>
-        
-        <div class="callout">
-            <h4 class = "subheader">About the technology</h4>
-            <p style="margin-left: 1em;">
 
-            </p>
-        </div>
-        
-        <div class="callout">
-            <h4 class = "subheader">What I've learned</h4>
-            <p style="margin-left: 1em;">
-                A big thing I learned was how to troubleshoot. It's hard to tell what went wrong when something isn't working, but 
-            </p>
+        <div class="cell">
+            <div class="callout">
+                <h4 class="subheader">About the creator</h4>
+                <p style="margin-left: 1em;">
+                    Hello! My name is Zoe and I'm a computer science major. I'm 26 years old and I plan to graduate either this summer or fall. In my free time, I like to draw and play video games. 
+                </p>
+            </div>
+
+            <div class="callout">
+                <h4 class="subheader">About the project</h4>
+                <p style="margin-left: 1em;">
+                    This project is a petsite, where a user can create and take care of virtual pets! I chose this project because I grew up on petsites, but most of them have been taken offline or were heavily dependent on Flash and Java. I thought it would be cool to bring modern functionalities to the concept. In the future, I'd love to add games and more pets and items.
+                </p>
+            </div>
+
+            <div class="callout">
+                <h4 class="subheader">About the technology</h4>
+                <p style="margin-left: 1em;">
+                    <h5 class="subheader">jQuery</h4> - javascript library used to handle events, ajax requests, and simplifies selecting objects from the DOM
+                    <h5 class="subheader">jQuery-confirm</h4> - jQuery plugin that provides dialogs with tons of useful features
+                    <h5 class="subheader">anime.js</h4> - javascript library which provides powerful animation methods
+                </p>
+            </div>
+
+            <div class="callout">
+                <h4 class="subheader">What I've learned</h4>
+                <p style="margin-left: 1em;">
+                    I learned a lot about troubleshooting. Things would constantly break, and at first I wasn't sure how to go about fixing things. I figured out how to identify the problem and not spend too much time messing with various things till it worked.
+                </p>
+            </div>
         </div>
     </div>
 </div>
+
 <?php
     send_footer();
 ?>
